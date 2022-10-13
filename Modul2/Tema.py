f4de4c3468bdce7019c25eef01763f0ec0e84fa2
haiduc <viorel.haiduc83@gmail.com>
2022-10-13 17:20:19 +0300

    tema

---
+++ Modul2/Tema.py
@@ -0,0 +1,18 @@
+#1
+#a="Passme1n"
+#b=input("Introduceti parola:" )
+#print(a==b)
+
+#2
+a=input("Introduceti primul nume")
+b=input("Introduceti al doilea nume")
+print(len (a))
+print(a==b)
+if len(a) > len(b):
+    print("Primul nume este mai lung decat al doilea")
+else:
+    print("Primul nume e mai scurt decat al doilea")
+print(a[0])
+print(a[::-1])
+
+#3
