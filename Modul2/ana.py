6b4efd6fd8582a55fff0fa25325272687e6032ef
haiduc <viorel.haiduc83@gmail.com>
2022-10-18 21:37:43 +0300

    1

--- Modul2/ana.py
+++ Modul2/ana.py
@@ -9,3 +9,13 @@ print(a + '.', b.replace(' ', '_'), c, '.',sep='_')
 print(a.split()[0]*4, a.split()[1], b.replace(' ', '_'), c, sep='_')
 x, y = a.split()
 print(y, x)
+
+
+a = 5.
+b = 5
+c = "ana"
+print(id(a), id(b), id(c))
+print(hex(id(a)), hex(id(b)), hex(id(c)))
+print('Type of a is: ', type(a),
+      'Type of b is: ', type(b),
+      'Type of c is: ', type(c))
